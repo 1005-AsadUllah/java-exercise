9c3bcdd1393b76465464797c15c248ae5ad1438f
Asad_Ullah <au10050305@gmail.com>
2025-02-26 23:42:46 +0600

    add 1,2,3,4,5

--- Collection FramWork/hashSet.java
+++ Collection FramWork/hashSet.java
@@ -1,7 +1,22 @@
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+
+
+
 
 public class hashSet {
     public static void main(String[] args) {
-        
+        Collection<String> strings = List.of("one", "two", "three", "four");
+
+for (Iterator<String> iterator = strings.iterator(); iterator.hasNext();) {
+    String element = iterator.next();
+    if (element.length() == 3) {
+        System.out.println(element);
+    }
+}
+
     }
 }
